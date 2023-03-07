52426f4f71ce046f451e00550286ed815b74ed90
Airton <airton.manfrini@hotmail.com>
2023-03-06 21:36:25 -0300

    add: nothing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToolbarModule } from 'primeng/toolbar';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { FeaturesModule } from '../features/features.module';
+import { SideNavService } from '../shared/services/side-nav/side-nav.service';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
-import { FooterComponent } from './components/footer/footer.component';
-import {ButtonModule} from 'primeng/button';
-import {SidebarModule} from 'primeng/sidebar';
-import {ToolbarModule} from 'primeng/toolbar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule} from '@angular/material/list';
-import { AppRoutingModule } from '../app-routing.module';
-import { SideNavService } from '../shared/services/side-nav/side-nav.service';
-import { FeaturesModule } from '../features/features.module';
 
 @NgModule({
   declarations: [
